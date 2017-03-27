7803993229ddac3cf97eb2797c6185db87c8630b
JJ <jbernhardt@fwparker.org>
2017-03-27 14:29:55 -0500

    Homework Done

--- twitter_scrape.py
+++ twitter_scrape.py
@@ -5,3 +5,17 @@
 # Use BeautifulSoup and urllib to grab the text contents of the tweets
 # located on the twitter page you chose.  The .text attribute will supply the content of a soup object.
 # Have fun.  Again, nothing explicit. (15pts)
+import urllib.request
+from bs4 import BeautifulSoup
+
+target = "Michael_Daalder"
+
+line = "\n--------------------------------------\n"
+print(target + "'s Twitter Feed!\n", line)
+for i in [[[y.text.strip() for y in x.findAll("p", {"class": "TweetTextSize"})],
+           [y.text.strip() for y in x.findAll("a", {"class": "account-group"})],
+           [y.text.strip() for y in x.findAll("small", {"class": "time"})]] for x in
+          BeautifulSoup(urllib.request.urlopen("https://twitter.com/" + target).read(), "lxml").find("ol", {
+              "class": "stream-items"}).findAll("li", {"class": "stream-item"})][:-1]:
+    print("\033[1mOn", i[2][0] + ",", i[1][0], "said:\033[0m")
+    print(i[0][0] + "\n", line)
--- weather_scrape.py
+++ weather_scrape.py
@@ -1,12 +1,23 @@
-
 # Below is a link to a 10-day weather forecast at weather.com
+url = "https://weather.com/weather/tenday/l/USIL0225:1:US"
 # Use urllib and BeautifulSoup to scrape data from the weather table.
+import urllib.request
+from bs4 import BeautifulSoup
+
 # Print a brief synopsis of the weather for the next 10 days.
 # Include the day, date, high temp, low temp, and chance of rain.
 # You can customize the text as you like, but it should be readable
 # for the user.  You will need to target specific classes or other
 # attributes to pull some parts of the data.
 # (e.g.  Wednesday, March 22: the high temp will be 48 with a low of 35, and a 20% chance of rain). (25pts)
-
-
-url = "https://weather.com/weather/tenday/l/USIL0225:1:US"
+page = urllib.request.urlopen(url)
+soup = BeautifulSoup(page.read(), "lxml")
+if soup.find("table"):
+    data_list = [[y.text.strip() for y in x.findAll("td")][1:] for x in soup.find("table").findAll("tr")][1:]
+    data_list[0][0] = "Now" + data_list[0][0][5:]
+    for day in data_list:
+        print(day[0][:3] + ", " + day[0][3:] + ": will be " + day[1] + ". The high temp will be " +
+              day[2].split("°")[0] + " with a low of " +
+              day[2].split("°")[0] + ", and a " + day[3] + " chance of rain.")
+else:
+    print("Try again -- your connection is bad.")
