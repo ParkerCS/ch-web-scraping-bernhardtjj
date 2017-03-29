d3f1d2db21a8b34f6b97d34d5374e0974a30663f
JJ <jbernhardt@fwparker.org>
2017-03-29 10:02:17 -0500

    Prettify x40

--- twitter_scrape.py
+++ twitter_scrape.py
@@ -9,11 +9,13 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 target = "Michael_Daalder"
+# target = "JohnIaconianni"
 
 line = "\n--------------------------------------\n"
 print(target + "'s Twitter Feed!\n", line)
-for i in [[[y.text.strip() for y in x.findAll("p", {"class": "TweetTextSize"})],
-           [y.text.strip().replace("Verified account", "") for y in x.findAll("a", {"class": "account-group"})],
+for i in [[[y.text.strip().split() for y in x.findAll("p", {"class": "TweetTextSize"})],
+           [y.text.strip().replace("Verified account", "").replace("@", "\033[36m@") + "\033[1m" for y in
+            x.findAll("a", {"class": "account-group"})],
            [y.text.strip()[y.text.strip().find([s for s in y.text.strip() if not s.isdigit()][0]):] for y in
             x.findAll("small", {"class": "time"})]] for x in
           BeautifulSoup(urllib.request.urlopen("https://twitter.com/" + target).read(), "lxml").find("ol", {
@@ -22,5 +24,12 @@ for i in [[[y.text.strip() for y in x.findAll("p", {"class": "TweetTextSize"})],
         i[2][0] = "About " + i[2][0][1:]
     else:
         i[2][0] = "On " + i[2][0]
+    tt = ''
+    for n in i[0][0]:
+        if n[0] == '#':
+            n = "\033[92m" + n + "\033[0m"
+        elif n[0] == '@':
+            n = "\033[36m" + n + "\033[0m"
+        tt += (n + " ")
     print("\033[1m" + i[2][0] + ",", i[1][0], "said:\033[0m")
-    print(i[0][0] + "\n", line)
+    print(tt.replace("pic.twitter.com", "\033[94m\nimage at http://pic.twitter.com") + "\033[0m\n", line)
